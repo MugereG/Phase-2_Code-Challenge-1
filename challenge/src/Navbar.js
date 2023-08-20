005894ad612558ea52abb0b5c0e0f758b9f36def
Gideon Mugere <gideonnemugere@gmail.com>
2023-08-20 07:33:38 +0300

    Ongoing Assignment

---
+++ challenge/src/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Navbar(){
+    return(
+        <div className='Container'>
+            <div>
+                <h1>Hello World</h1>
+            </div>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Login</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
